a636c44b1150c3284c86d7d58d02807e216f9016
jb <jbuverud@gmail.com>
2021-03-05 10:48:28 +0000

    Add try/catch around jsQR call, ignore RangeError-errors sometimes thrown on iOS

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,34 @@
 // jsQR is concatenated by gulp
 
-self.addEventListener('message', function(e) {
-  var decoded = jsQR(
-    e.data.data,
-    e.data.width,
-    e.data.height
-  )
-  if (decoded) {
-    postMessage(decoded.data)
-  } else {
-    postMessage(null)
-  }
+self.addEventListener('message', function (e) {
+
+    var decoded = jsQR(
+        e.data.data,
+        e.data.width,
+        e.data.height
+    )
+    if (decoded) {
+        postMessage(decoded.data)
+    } else {
+        postMessage(null)
+    }
+
+    try {
+        var decoded = jsQR(
+            e.data.data,
+            e.data.width,
+            e.data.height
+        )
+        if (decoded) {
+            postMessage(decoded.data)
+        } else {
+            postMessage(null)
+        }
+
+    } catch (error) {
+        if (!(error instanceof RangeError)) {
+            throw error;
+        }
+        postMessage(null)
+    }
 })
